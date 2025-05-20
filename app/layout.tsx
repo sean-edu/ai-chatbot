0342fbf93c9536fd6183c09235fa579126e913c0
sean-edu <lsh2078@gmail.com>
2025-05-20 14:16:09 +0900

    Update layout.tsx

--- app/layout.tsx
+++ app/layout.tsx
@@ -30,7 +30,7 @@ const geistMono = Geist_Mono({
 
 const LIGHT_THEME_COLOR = 'hsl(0 0% 100%)';
 const DARK_THEME_COLOR = 'hsl(240deg 10% 3.92%)';
-const THEME_COLOR_SCRIPT =
+const THEME_COLOR_SCRIPT = `\
 (function() {
   var html = document.documentElement;
   var meta = document.querySelector('meta[name="theme-color"]');
@@ -46,7 +46,7 @@ const THEME_COLOR_SCRIPT =
   var observer = new MutationObserver(updateThemeColor);
   observer.observe(html, { attributes: true, attributeFilter: ['class'] });
   updateThemeColor();
-})();;
+})();`;
 
 export default async function RootLayout({
   children,
@@ -56,12 +56,12 @@ export default async function RootLayout({
   return (
     <html
       lang="en"
-      // next-themes injects an extra classname to the body element to avoid
-      // visual flicker before hydration. Hence the suppressHydrationWarning
+      // `next-themes` injects an extra classname to the body element to avoid
+      // visual flicker before hydration. Hence the `suppressHydrationWarning`
       // prop is necessary to avoid the React hydration mismatch warning.
       // https://github.com/pacocoursey/next-themes?tab=readme-ov-file#with-app
       suppressHydrationWarning
-      className={${geist.variable} ${geistMono.variable}}
+      className={`${geist.variable} ${geistMono.variable}`}
     >
       <head>
         <script
